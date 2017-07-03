a4fdde88168e6c384b18d62d0fc6ed58e35164ac
root <root@puppetmaster.lan>
2017-07-03 13:47:29 +0000

    remove comments in parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,14 @@
 class ntp(
-
-  # Install parameters are first:
   $package_name,
   $package_ensure,
-  
-  # Configuration parameters are next:
   $configuration_name,
   $configuration_file_mode,
   $servers,
-  
-  # Service parameters are last:
   $service_ensure,
   $service_name,
   $service_enable,
   $service_hasrestart,
   $service_hasstatus,  
-
 ) {
   class { '::ntp::install': }
   -> class { '::ntp::config': }
