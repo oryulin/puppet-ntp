eb0dba4627a4bc506a22190ecce5c2c4448da17b
root <root@puppetmaster.lan>
2017-07-03 15:25:20 +0000

    bugs

--- functions/data.pp
+++ functions/data.pp
@@ -1,9 +1,7 @@
-# Defining data function which replaces the deprecated params.pp file:
 function ntp::data {
-
   $base_params = {  
     'ntp::package_name'            => 'ntp',
-    'ntp::package_ensure'          => present,
+    'ntp::package_ensure'          => 'present',
     'ntp::configuration_name'      => 'ntp.conf',
     'ntp::configuration_file_mode' => '0644',
     'ntp::servers'                 => ['0.centos.pool.ntp.org','1.centos.pool.ntp.org'],
@@ -12,7 +10,6 @@ function ntp::data {
     'ntp::service_hasrestart'      => true,
     'ntp::service_hasstatus'       => true,
   }
-
   $os_params = $facts['os']['family'] ? {
     'Debian' => { 
       'ntp::service_name'          => 'ntp',
@@ -21,6 +18,5 @@ function ntp::data {
       'ntp::service_name'          => 'ntpd',
     },
   }
-  
   $base_params + $os_params
 }
